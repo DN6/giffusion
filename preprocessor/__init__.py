9063929bb69b8de85b49ce8dbb007a8d9fc233fa
DN6 <dhruv.nair@gmail.com>
2023-05-08 11:59:31 +0530

    remove scaling in preprocessing

--- preprocessor/__init__.py
+++ preprocessor/__init__.py
@@ -7,13 +7,11 @@ from torchvision.transforms import ToPILImage, ToTensor
 
 def apply_canny(image):
     _, transformed = canny(image)
-    transformed /= 255.0
 
     return transformed
 
 
 def apply_inpainting_mask(image):
-    image /= 255.0
     output = torch.where(image < 0.0, -1.0, image)
 
     return output
