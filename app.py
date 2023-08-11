adea5c5d1a2403daf37adde3daddaf43629e696a
Dhruv Nair <dhruv.nair@gmail.com>
2023-08-11 18:50:28 +0530

    support for custom pipelines (#32)
    * fix precision issues
    
    * add support for custom pipelines
    
    * update readme
    
    * small fix for sdxl controlnet

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ CONTROLNET_PROCESSORS = ["no-processing"] + list(CONTROLNET_PROCESSORS.keys())
 prompt_generator = gr.Interface.load("spaces/doevent/prompt-generator")
 
 
-def load_pipeline(model_name, pipeline_name, controlnet, pipe):
+def load_pipeline(model_name, pipeline_name, controlnet, custom_pipeline, pipe):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     try:
@@ -41,36 +41,44 @@ def load_pipeline(model_name, pipeline_name, controlnet, pipe):
         success_message = (
             f"Successfully loaded Pipeline: {pipeline_name} with {model_name}"
         )
+        pipe_cls = getattr(importlib.import_module("diffusers"), pipeline_name)
+
         if controlnet:
             from diffusers import ControlNetModel
 
             controlnets = [controlnet.strip() for controlnet in controlnet.split(",")]
-            controlnet_models = [
-                ControlNetModel.from_pretrained(
-                    controlnet, torch_dtype=torch.float16, cache_dir=MODEL_PATH
-                )
-                for controlnet in controlnets
-            ]
 
-            pipeline_name = "StableDiffusionControlNetPipeline"
-            pipe_cls = getattr(importlib.import_module("diffusers"), pipeline_name)
+            # temporary solution to multicontrolnet issue in sdxl
+            if len(controlnets) == 1:
+                controlnet_models = ControlNetModel.from_pretrained(
+                    controlnets[0], torch_dtype=torch.float16, cache_dir=MODEL_PATH
+                )
+            else:
+                controlnet_models = [
+                    ControlNetModel.from_pretrained(
+                        controlnet, torch_dtype=torch.float16, cache_dir=MODEL_PATH
+                    )
+                    for controlnet in controlnets
+                ]
+
             pipe = pipe_cls.from_pretrained(
                 model_name,
                 torch_dtype=torch.float16,
                 safety_checker=None,
                 controlnet=controlnet_models,
                 cache_dir=MODEL_PATH,
+                custom_pipeline=custom_pipeline if custom_pipeline else None,
             )
             success_message = f"Successfully loaded Pipeline: {pipeline_name} with {model_name} and {controlnets}"
 
         else:
-            pipe_cls = getattr(importlib.import_module("diffusers"), pipeline_name)
             pipe = pipe_cls.from_pretrained(
                 model_name,
                 use_auth_token=True,
                 torch_dtype=torch.float16,
                 safety_checker=None,
                 cache_dir=MODEL_PATH,
+                custom_pipeline=custom_pipeline if custom_pipeline else None,
             )
 
         if hasattr(pipe, "enable_model_cpu_offload"):
@@ -231,6 +239,7 @@ with demo:
                             label="Pipeline Name", value="DiffusionPipeline"
                         )
                         controlnet = gr.Textbox(label="ControlNet Checkpoint")
+                        custom_pipeline = gr.Textbox(label="Custom Pipeline")
                     with gr.Column():
                         with gr.Row():
                             load_pipeline_btn = gr.Button(value="Load Pipeline")
@@ -434,7 +443,7 @@ with demo:
 
     load_pipeline_btn.click(
         load_pipeline,
-        [model_name, pipeline_name, controlnet, pipe],
+        [model_name, pipeline_name, controlnet, custom_pipeline, pipe],
         [pipe, load_message],
     )
 
