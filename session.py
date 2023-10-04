9e84420681595304942ba96496ed94b566afe44b
DN6 <dhruv.nair@gmail.com>
2023-10-04 21:22:31 +0200

    add sessions

---
+++ session.py
@@ -0,0 +1,17 @@
+from huggingface_hub import HfApi, HfHubHTTPError, create_repo
+
+api = HfApi()
+
+
+def save_session(path, repo_id):
+    try:
+        repo_url = create_repo(repo_id, private=True)
+    except HfHubHTTPError as e:
+        repo_url = None
+
+    api.upload_folder(
+        folder_path=path,
+        path_in_repo=path.split("/")[-1],
+        repo_id=repo_id,
+        repo_type="dataset",
+    )
